6aa3e99ec610fc3a854785c3eedd91108b0a27bc
RokitG <pintosdsgn@gmail.com>
2025-04-13 18:34:22 +0700

    Release v1.0.1

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -7,7 +7,4 @@ const bot = new Telegraf(BOT_TOKEN);
 // Configure the bot to use a shorter polling timeout to respond quicker to shutdown signals
 bot.telegram.getUpdates(0, 100, 10, []); // (offset, limit, timeout, allowed_updates)
 
-// Remove individual signal handlers
-// They're now handled centrally in index.ts
-
 export { bot };
